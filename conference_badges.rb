7d4b0c9814b6a1615998ab30c08dabada4e5f5db
eyi1 <ej7729@gmail.com>
2018-03-28 01:59:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,7 @@ end
 
 
 def assign_rooms (attendee)
+  room_number = (1...7).to_a
   attendees.each_with_index { |attendee, index| "Hello, #{attendee}!
      You'll be assigned to room #{room_number[index]}!" }
 end
