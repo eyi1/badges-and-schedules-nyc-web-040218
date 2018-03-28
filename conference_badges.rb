d93f99de055fe6427351f758f57bb0bc8ab646fc
eyi1 <ej7729@gmail.com>
2018-03-28 02:09:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ end
 
 
 def assign_rooms (speakers)
-  room_number = (1...7).to_a
-  speakers.each_with_index {|speaker, index| room_number << "Hello, #{speaker}! You'll be assigned to room #{[index]}!" }
+  room_number = []
+  speakers.each_with_index {|speaker, index| room_number << "Hello, #{speaker}! You'll be assigned to room #{index+1}!" }
      return room_number
 end
